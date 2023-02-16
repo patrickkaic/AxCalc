6e92ca20a3865f6583f1db3f050de8981a461b5c
Patrick Almeida <patrickkaic@gmail.com>
2023-02-16 19:56:35 -0300

    modificações nas linhas 21 em diante

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,12 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final items = [
+    'Anticoagulantes e Anestesia no Neuroeixo',
+    'Balanço Hídrico Simplificado',
+    'Variação do Volume Sistólico',
+    'Índice de Massa Corporal',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +18,58 @@ class HomePage extends StatelessWidget {
         title: Text('AxCalc'),
         centerTitle: true,
       ),
-      body: ListView(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          ListTile(
-            title: Text('Indice de massa corporal'),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => UserInputPage(title: items[index])),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class UserInputPage extends StatefulWidget {
+  final String title;
+
+  UserInputPage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _UserInputPageState createState() => _UserInputPageState();
+}
+
+class _UserInputPageState extends State<UserInputPage> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: _textEditingController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Insira aqui sua entrada de usuário',
           ),
-          ListTile(
-            title: Text('ml para litros'),
-          ),
-        ],
+        ),
       ),
     );
   }
