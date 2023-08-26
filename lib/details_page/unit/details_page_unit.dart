31ebdcea804c93119928895fd768ecfa8043f010
patrickkaic <patrickkaic@gmail.com>
2023-08-25 23:16:02 -0300

    fix: started fix in unit screen

--- lib/details_page/unit/details_page_unit.dart
+++ lib/details_page/unit/details_page_unit.dart
@@ -161,6 +161,57 @@ class _detailsPageUnitState extends State<detailsPageUnit> {
               ],
             ),
           ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'CONCENTRAÇÃO DA SOLUÇÃO',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 169, 167, 167),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 3),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: inputController1,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 202, 200, 200),
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      suffixIcon: Align(
+                        widthFactor: 1.0,
+                        heightFactor: 1.0,
+                        child: Text(
+                          '%',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 151, 149, 149),
+                          ),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 202, 200, 200)),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           SizedBox(
             width: 120,
             height: 100,
