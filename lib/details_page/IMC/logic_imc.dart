e5be6ff888434583ac020a6228d1ea1e3812b5a7
patrickkaic <patrickkaic@gmail.com>
2023-08-28 23:09:51 -0300

    new function addded

--- lib/details_page/IMC/logic_imc.dart
+++ lib/details_page/IMC/logic_imc.dart
@@ -4,3 +4,16 @@ class CalculadoraIMC {
     return imc.toStringAsFixed(2);
   }
 }
+
+String results(double imc) {
+  if (imc < 18.5) {
+    return "Abaixo do peso";
+  }
+  if (imc < 24.9) {
+    return "Normal";
+  }
+  if (imc < 29.9) {
+    return "Sobrepeso";
+  }
+  return "Obesidade";
+}
