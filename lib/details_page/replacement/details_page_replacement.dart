f8b0fb490d23d6990ae5dbe79d2c3dbdb631a7e1
patrickkaic <patrickkaic@gmail.com>
2023-08-30 16:30:30 -0300

    fixes in dropdownbutton

--- lib/details_page/replacement/details_page_replacement.dart
+++ lib/details_page/replacement/details_page_replacement.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, must_be_immutable
 
 import 'package:flutter/material.dart';
 
