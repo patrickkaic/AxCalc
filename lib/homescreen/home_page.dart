bd2c139ce8efbb8b5a187d41ed82ac21919c02ff
patrickkaic <patrickkaic@gmail.com>
2023-09-14 13:50:46 -0300

    fix: searchbar now is working

--- lib/homescreen/home_page.dart
+++ lib/homescreen/home_page.dart
@@ -4,10 +4,37 @@ import 'package:axcalc/homescreen/my_cards.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final TextEditingController _searchController = TextEditingController();
+
+  final myCards = [
+    {'text': 'IMC, Peso Ideal e Corrigido', 'url': '/detalhesimc'},
+    {'text': 'Reposição de Jejum', 'url': '/detalhesjejum'},
+    {'text': 'Taxa de Infusão em ml/h', 'url': '/detalhesinf'},
+    {'text': 'Conversor de Soluções', 'url': '/detalhessol'},
+  ];
+
+  late List<Map<String, dynamic>> myCardFilter = myCards;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      myCardFilter = myCards
+          .where((element) => element['text']!
+              .toUpperCase()
+              .contains(_searchController.text.toUpperCase()))
+          .toList();
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,62 +54,53 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-        children: [
-          Container(
-            constraints: BoxConstraints(maxHeight: 63),
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Buscar cálculo',
-                filled: true,
-                fillColor: Color.fromARGB(35, 88, 89, 92),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () => _searchController.clear(),
-                ),
-                prefixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: BorderSide(color: Colors.transparent),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: BorderSide(color: Colors.transparent),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Container(
+              constraints: BoxConstraints(maxHeight: 63),
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Buscar cálculo',
+                  filled: true,
+                  fillColor: Color.fromARGB(35, 88, 89, 92),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () => _searchController.clear(),
+                  ),
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: null,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
                 ),
               ),
             ),
-          ),
-          MyCards(
-            text: 'IMC, Peso Ideal e Corrigido',
-            onTap: () {
-              GoRouter.of(context).push('/detalhesimc');
-            },
-          ),
-          MyCards(
-            text: 'Reposição de Jejum',
-            onTap: () {
-              GoRouter.of(context).push('/detalhesjejum');
-            },
-          ),
-          MyCards(
-            text: 'Taxa de Infusão em ml/h',
-            onTap: () {
-              GoRouter.of(context).push('/detalhesinf');
-            },
-          ),
-          MyCards(
-            text: 'Conversor de Soluções',
-            onTap: () {
-              GoRouter.of(context).push('/detalhessol');
-            },
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                  itemCount: myCardFilter.length,
+                  itemBuilder: (context, index) {
+                    final card = myCardFilter[index];
+                    return MyCards(
+                      text: card['text']!,
+                      onTap: () {
+                        GoRouter.of(context).push(card['url']!);
+                      },
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
